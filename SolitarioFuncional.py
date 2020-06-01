3f5893da284564a4e2510ea5abdad36f1d86c30d
Maicol Andrés Garzón Fonseca <55245364+MikeGarzon@users.noreply.github.com>
2020-06-01 18:10:43 -0500

    Update SolitarioFuncional.py
    Version juego, no reparte jugador

--- SolitarioFuncional.py
+++ SolitarioFuncional.py
@@ -1,17 +1,18 @@
 from random import shuffle
 
 def baraja():
-    return [(n, p) for n in (['A', 'J', 'Q', 'K'] +[str(x) for x in range (2,11)]) for p in ['♠','♥','♣','♦']]
+    return [(n, p) for n in ['A', 'J', 'Q', 'K'] +[ str(x) for x in range (2,11)] for p in ['♥','♣','♦','♠']]
 
 def mezclar(baraja):
-    shuffle (baraja)
+    shuffle(baraja)
     return baraja
 
-def sacar_carta(baraja):
-    if baraja == []:
+def sacar_carta(mazo):
+    if mazo == []:
         pass
     else:
-        print (baraja[0], valor_carta(baraja[0]))
+        print(mazo[0], valor_carta(mazo[0]))
+        sacar_carta(mazo[1:])
 
 def valor_carta(carta):
     if carta[0] in ['J', 'Q', 'K']:
@@ -24,16 +25,41 @@ def valor_carta(carta):
 def valor_mano(mano):
     if mano == []:
         return 0
-    else:
-        print(mano[0]) 
-        return valor_carta(mano[0]) + valor_mano(mano[1:])
+    return valor_carta(mano[0]) + valor_mano(mano[1:])
 
-def valor_mano_recargado(mano):
-    print (mano)
-    if valor_mano(mano) <=11 and 1 in [valor_carta(x) for x in mano]:
+def valor_juego(mano):
+    if valor_mano(mano) <= 11 and 1 in [valor_carta(x) for x in mano]:
         return valor_mano(mano) + 10
     else:
         return valor_mano(mano)
 
+def repartir_mano_dealer(mazo, dealer, jugador):
+    if valor_juego(dealer) < 21 :
+        repartir_mano_dealer(mazo[1:], dealer+[mazo[0]], jugador)
+    elif valor_juego(jugador) <= valor_juego(dealer) or valor_juego(jugador) < 21:
+        print ("cartas Dealer: \n" +str(dealer))
+        ganador_es(valor_juego(jugador), valor_juego(dealer))
 
-sacar_carta(mezclar(baraja()))
+def ganador_es(jugador, dealer):
+    if jugador > dealer :
+        print("Jugador Gana")
+        return False
+    elif dealer > 21:
+        print("Jugador Gana")
+        return False
+    elif jugador > 21:
+        print("Dealer Gana")
+        return False
+    else:
+        print("Dealer Gana")
+        return False
+
+def jugar(mazo, jugador, dealer):
+    print(valor_juego(jugador))
+    if len(mazo) > 2 and valor_juego(jugador) < 21 and input("Presione p para pedir carta, si no presione Enter: "):
+        jugar(mazo[2:], jugador+[mazo[0]], dealer)
+    else:
+        print("Cartas Jugador: \n" + str(jugador))
+        repartir_mano_dealer(mazo, dealer, jugador )
+
+print(jugar(mezclar(baraja()),[],[]))
